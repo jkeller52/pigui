968a2e97e6d3ff00495380458016b89da790f38d
jkeller52 <jacobkeller52@gmail.com>
2021-02-12 07:55:14 -0500

    changes

--- pause.sh
+++ pause.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PATH=/bin:/usr/bin:/usr/local/bin
-ssh -T jacobkeller@mbp.wowway.com '/Users/jacobkeller/Documents/GitHub/pigui/pause.sh'
+ssh -T jacobkeller@mbp.wowway.com '/Users/jacobkeller/Documents/GitHub/pigui/pausebtn.sh'
