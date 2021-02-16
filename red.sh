d661b1c6bafde90dad11bc0e19ccde2884fe6934
jkeller52 <jacobkeller52@gmail.com>
2021-02-16 01:57:34 -0500

    test

--- red.sh
+++ red.sh
@@ -3,4 +3,4 @@ export PATH=/bin:/usr/bin:/usr/local/bin
 #ssh -T  'python3 /Users/jacobkeller/Documents/GitHub/pigui/redbtn.py'
 
 
-ssh -T jacobkeller@mbp.wowway.com '. ~/.bashrc; /Users/jacobkeller/Documents/GitHub/pigui/redbtn.py'
+ssh -T jacobkeller@mbp.wowway.com 'sudo . ~/.bashrc; /Users/jacobkeller/Documents/GitHub/pigui/redbtn.py'
