5fe3b2a9f1144687eeb3139c0680f722d2a6bb95
jkeller52 <jacobkeller52@gmail.com>
2021-02-16 20:36:35 -0500

    test scene.sh

--- scene.sh
+++ scene.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #export PATH=/bin:/usr/bin:/usr/local/bin
-ssh -T jacobkeller@mbp.wowway.com 'python3 /Users/jacobkeller/Documents/GitHub/pigui/scenebtn.py'
+python3 /home/pi/pigui/scenebtn.py
