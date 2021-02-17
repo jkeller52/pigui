443a2c9102f9ef4bf02a210eab772f5f6dcc9839
jkeller52 <jacobkeller52@gmail.com>
2021-02-16 21:08:20 -0500

    Update colourbtn.py

--- colourbtn.py
+++ colourbtn.py
@@ -22,5 +22,25 @@ r.set_colour(255, 94, 0)
 l = tinytuya.BulbDevice('eb71194515a20f5c11qxac', '192.168.0.29', 'e94a74a5ab6cd3f7')
 l.set_version(3.3)  # IMPORTANT to set this regardless of version
 ldata = l.status()
-l.set_mode(mode='colour') # white, colour, scene, music
-l.set_colour(0, 0, 200)
+l.set_mode(mode='white') # white, colour, scene, music
+l.set_brightness_percentage(brightness=35)
+
+
+ #BulbDevice
+  #      set_colour(r, g, b):
+  #      set_white(brightness, colourtemp):
+  #      set_white_percentage(brightness=100, colourtemp=0):
+  #      set_brightness(brightness):     # Brightness: Type A devices range = 25-255 and Type B = 10-1000
+  #      set_brightness_percentage(brightness=100):
+  #      set_colourtemp(colourtemp):
+  #      set_colourtemp_percentage(colourtemp=100):
+  #      set_scene(scene):             # 1=nature, 3=rave, 4=rainbow
+  #      set_mode(mode='white'):       # white, colour, scene, music
+  #      result = brightness():
+  #      result = colourtemp():
+  #      (r, g, b) = colour_rgb():
+  #      (h,s,v) = colour_hsv()
+  #      result = state():
+
+
+  #rl = tinytuya.BulbDevice('device id', '192.168.0.11', 'local key')
